f0ae48a1055f6df020e0ed27cbe83a9d91f08769
chenyanliang <1419073319@qq.com>
2022-11-21 18:33:55 +0800

    测试页coding，测试step_widget位置

--- lib/main.dart
+++ lib/main.dart
@@ -13,92 +13,138 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       navigatorObservers: [MaskGuideNavigatorObserver()],
-      home: const FirstPage(),
+      home: FirstPage(),
     );
   }
 }
 
-class FirstPage extends StatelessWidget {
+class FirstPage extends StatefulWidget {
   const FirstPage({Key? key}) : super(key: key);
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  bool autoStart = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('First Page')),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(builder: (_) => const MyHomePage(title: 'Flutter Demo Home Page')));
-          },
-          child: const Text('跳转'),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(builder: (_) => MyHomePage(autoStart: autoStart)));
+              },
+              child: const Text('Start'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Auto Show:'),
+                Switch(value: autoStart, onChanged: (value) {
+                  setState(() {
+                    autoStart = value;
+                  });
+                }),
+              ],
+            )
+          ],
         ),
       ),
     );
   }
 }
 
-
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  const MyHomePage({super.key, required this.autoStart});
 
-  final String title;
+  final bool autoStart;
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final GlobalKey appBarKey = GlobalKey();
-  final GlobalKey textKey = GlobalKey();
-  final GlobalKey countKey = GlobalKey();
-  final GlobalKey buttonKey = GlobalKey();
+  final GlobalKey redKey = GlobalKey();
+  final GlobalKey yellowKey = GlobalKey();
+  final GlobalKey blueKey = GlobalKey();
+  final GlobalKey greenKey = GlobalKey();
 
   final MaskGuide maskGuide = MaskGuide();
 
-  bool canPop = false;
+  bool pop = false;
   bool prevent = false;
 
-  void showMaskGuide() {
-    prevent = false;
+  void showMaskGuide({required bool canPop, required bool canDismiss}) {
+    if (canPop) {
+      prevent = false;
+      pop = true;
+    } else {
+      prevent = true;
+      pop = false;
+    }
     maskGuide.showMaskGuide(
       context: context,
-      keys: [appBarKey, textKey, countKey, buttonKey],
+      keys: [redKey, yellowKey, blueKey, greenKey],
       guideTexts: ['这是第一个' * 10, '这是第二个', '这是第三个', '这是第四个'],
-      canPop: true,
+      canPop: canPop,
+      canDismiss: canDismiss,
       doneCallBack: () {
+        prevent = false;
+        pop = true;
         print('default done');
+      },
+      dismissCallBack: () {
+        prevent = false;
+        pop = true;
+        print('default dismiss');
       }
     );
   }
 
-  void showCustomMaskGuide() {
-    prevent = true;
+  void showCustomMaskGuide({required bool canPop, required bool canDismiss}) {
+    if (canPop) {
+      prevent = false;
+      pop = true;
+    } else {
+      prevent = true;
+      pop = false;
+    }
     maskGuide.showMaskGuide(
       context: context,
-      keys: [appBarKey, textKey, countKey, buttonKey],
+      keys: [redKey, yellowKey, blueKey, greenKey],
       customStepWidget: CustomStepWidget(
-        keys: [appBarKey, textKey, countKey, buttonKey],
+        keys: [redKey, yellowKey, blueKey, greenKey],
       ),
-      canDismiss: true,
+      canPop: canPop,
+      canDismiss: canDismiss,
+      doneCallBack: () {
+        prevent = false;
+        pop = true;
+        print('custom done');
+      },
       dismissCallBack: () {
         prevent = false;
+        pop = true;
+        print('custom dismiss');
       },
-      doneCallBack: () {
-        canPop = true;
-        print('custom done');
-      }
     );
   }
 
   @override
   void initState() {
     super.initState();
-    // WidgetsBinding.instance.addPostFrameCallback((_) => showMaskGuide());
+    if (widget.autoStart) {
+      WidgetsBinding.instance.addPostFrameCallback((_) => showMaskGuide(canPop: false, canDismiss: false));
+    }
   }
 
   @override
@@ -106,40 +152,110 @@ class _MyHomePageState extends State<MyHomePage> {
     return WillPopScope(
       onWillPop: () {
         if (prevent) {
-          return Future.value(canPop);
+          return Future.value(pop);
         } else {
           return Future.value(true);
         }
       },
       child: Scaffold(
         appBar: AppBar(
-          key: appBarKey,
-          title: Text(widget.title),
+          title: const Text('Home Page'),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              InkWell(
-                onTap: () => showCustomMaskGuide(),
-                child: const Text('Use custom step widget'),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      showMaskGuide(canPop: false, canDismiss: false);
+                    },
+                    child: const Text("Default, can't dismiss, can't pop"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      showMaskGuide(canPop: true, canDismiss: false);
+                    },
+                    child: const Text("Default, can't dismiss, can pop"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      showMaskGuide(canPop: false, canDismiss: true);
+                    },
+                    child: const Text("Default, can dismiss, can't pop"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      showMaskGuide(canPop: true, canDismiss: true);
+                    },
+                    child: const Text("Default, can dismiss, can pop"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      showCustomMaskGuide(canPop: false, canDismiss: false);
+                    },
+                    child: const Text("Custom, can't dismiss, can't pop"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      showCustomMaskGuide(canPop: true, canDismiss: false);
+                    },
+                    child: const Text("Custom, can't dismiss, can pop"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      showCustomMaskGuide(canPop: false, canDismiss: true);
+                    },
+                    child: const Text("Custom, can dismiss, can't pop"),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      showCustomMaskGuide(canPop: true, canDismiss: true);
+                    },
+                    child: const Text("Custom, can dismiss, can pop"),
+                  ),
+                ],
               ),
-              const SizedBox(height: 10),
-              Text(
-                'You have pushed the button this many times:',
-                key: textKey,
-              ),
-              Text(
-                '0',
-                key: countKey,
-                style: Theme.of(context).textTheme.headline4,
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    key: redKey,
+                    width: 50,
+                    height: 50,
+                    color: Colors.red,
+                  ),
+                  const SizedBox(height: 30),
+                  Container(
+                    key: yellowKey,
+                    width: 50,
+                    height: 50,
+                    color: Colors.yellow,
+                  ),
+                  const SizedBox(height: 30),
+                  Container(
+                    key: blueKey,
+                    width: 50,
+                    height: 50,
+                    color: Colors.blue,
+                  ),
+                  const SizedBox(height: 30),
+                  Container(
+                    key: greenKey,
+                    width: 50,
+                    height: 50,
+                    color: Colors.green,
+                  ),
+                ],
               ),
             ],
           ),
         ),
         floatingActionButton: FloatingActionButton(
-          key: buttonKey,
-          onPressed: () => showMaskGuide(),
+          onPressed: () {},
           tooltip: 'Increment',
           child: const Icon(Icons.add),
         ),
